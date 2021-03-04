2b6dde5e7ccbe1dd812b2f5ac8830717abd88c38
uo271432 <uo271432@uniovi.es>
2021-03-04 20:17:59 +0100

    Inicio

---
+++ sdi-entrega1-2021-601-2021-614/src/main/java/com/uniovi/SdiEntrega120216012021614Application.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdiEntrega120216012021614Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdiEntrega120216012021614Application.class, args);
+	}
+
+}
