440e4138fae8de2c9b4722b70e7a3e523bac4d48
uo271432 <uo271432@uniovi.es>
2021-03-09 22:36:17 +0100

    Ver el Listado de ofertas propias. Sin Prueba

--- sdi-entrega1-2021-601-2021-614/src/main/java/com/uniovi/controllers/OffersController.java
+++ sdi-entrega1-2021-601-2021-614/src/main/java/com/uniovi/controllers/OffersController.java
@@ -1,19 +1,20 @@
 package com.uniovi.controllers;
 
 import java.security.Principal;
+import java.util.LinkedList;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import com.uniovi.entities.Offer;
 import com.uniovi.entities.User;
 import com.uniovi.services.OffersService;
@@ -38,16 +39,27 @@ public class OffersController {
 	}
 	
 	@RequestMapping(value = "/offer/add", method = RequestMethod.POST )
-	public String setOffer(Model model, @Validated Offer offer, BindingResult result) {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	public String setOffer(Model model, @Validated Offer offer, BindingResult result, Principal principal) {
 		addOfferFormValidator.validate(offer, result);
 		if (result.hasErrors()) {
 			return "/offer/add";
 		}
-		User activeUser = usersService.getUserByEmail(auth.getName());
-		offersService.addOffer(offer, activeUser);
+		String email = principal.getName();
+		User user = usersService.getUserByEmail(email);
+		offersService.addOffer(offer, user);
 		logger.info(String.format("Add item %s", offer.getTitulo()));
 		return "redirect:/home";
 	}
+	@RequestMapping(value = "/offer/mylist")
+	public String getOffersForUser(Model model ,Pageable pageable, Principal principal) {
+		String email = principal.getName();
+		User user = usersService.getUserByEmail(email);
+		Page<Offer> offer = new PageImpl<Offer>(new LinkedList<Offer>());
+		offer=offersService.findAllByUser(pageable,user);
+		model.addAttribute("myOffers",offer.getContent());
+		model.addAttribute("page", offer);
+		return "offer/mylist";
+	}
+	
 	
 }
--- sdi-entrega1-2021-601-2021-614/src/main/java/com/uniovi/services/OffersService.java
+++ sdi-entrega1-2021-601-2021-614/src/main/java/com/uniovi/services/OffersService.java
@@ -3,8 +3,11 @@ package com.uniovi.services;
 
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import com.uniovi.entities.Offer;
 import com.uniovi.entities.User;
@@ -26,6 +29,10 @@ public class OffersService {
 		user.getOffers().add(offer);
 		offersRepository.save(offer);
 		usersRepository.save(user);
+	}
+
+	public Page<Offer> findAllByUser(Pageable pageable,User user) {
+		return offersRepository.findAllByUser(pageable,user);
 	}
 
 }
